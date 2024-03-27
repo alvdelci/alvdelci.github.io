892e0050e8cdac0cd2b3dec730bba680282944f6
alvdelci <delcimarioalvesdev@gmail.com>
2024-03-27 10:12:59 -0300

    Novo script para monitoramento do scroll

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-function addActiveClass(id) {
-    console.log("na funcao");
-    let lastElement = document.querySelector(".active");
-    let element = document.getElementById(id);
+/**Funcoes de acoes durante scroll */
+let currentSection;
 
-    if (lastElement) {
-        lastElement.classList.remove("active");
-    }
-    element.classList.add("active");
-    element.scrollIntoView(true);
-
+// Função para obter o retângulo de uma seção
+function getBoundingClientRect(section) {
+    return section.getBoundingClientRect();
 }
+
+// Função para verificar se a seção está visível na tela
+function isSectionVisible(section) {
+    const rect = getBoundingClientRect(section);
+    const visibleHeight = rect.height * (rect.bottom / window.innerHeight);
+    return visibleHeight >= window.innerHeight / 2;
+}
+
+// Função para adicionar a classe "current" à seção predominante
+function addCurrentClassToPredominantSection() {
+    if (currentSection) {
+        const sumaryItem = document.getElementsByName(currentSection.id);
+        sumaryItem[0].classList.remove("active");
+        currentSection.classList.remove("current");
+    }
+
+    const sections = document.querySelectorAll(".section");
+
+    for (const section of sections) {
+        if (isSectionVisible(section)) {
+            currentSection = section;
+            break;
+        }
+    }
+
+    if (currentSection) {
+        const sumaryItem = document.getElementsByName(currentSection.id);
+        sumaryItem[0].classList.add("active");
+    }
+}
+
+// Adicionar evento de scroll à janela
+window.addEventListener("scroll", addCurrentClassToPredominantSection);
+
+// Chamar a função no carregamento da página
+addCurrentClassToPredominantSection();
